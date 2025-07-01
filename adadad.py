c1aef9c558a75f51639c43d88fe21d35b47060e2
cristopher-arch <crist.gomezv@duocuc.cl>
2025-07-01 14:54:47 -0400

    sumar arreglado

---
+++ adadad.py
@@ -0,0 +1,12 @@
+def sumar(a,b):
+    return a+b
+
+while True:
+    try:
+    
+        n1 = int(input("Ingrse valor: "))
+        n2 = int(input("Ingrese valor: "))
+        break
+    except:
+
+print(sumar(n1,n2))
